b19d160381599ee1b9cdba288c3df775f48f1f0e
aeiout <ueaarthon13117@gmail.com>
2023-01-13 17:09:27 +0700

    completed

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -17,3 +17,27 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+
+void stat(const double A[],int N,double B[]){
+    double sum=0,t=0,g=1,p=0,max=A[0],min=A[0];
+    for(int i=0;i<N;i++){
+        sum+=A[i];
+        g=g*A[i];
+        p+=1.0/A[i];
+        if(A[i]>max){
+            max=A[i];
+        }if(A[i]<min){
+            min=A[i];
+        }
+    }
+B[0]=sum/N;
+B[2]=pow(g,1.0/N);
+B[3]=N/p;
+B[4]=max;
+B[5]=min;
+
+    for(int i=0;i<N;i++){
+        t+=pow(A[i]-B[0],2);
+    }
+B[1]=sqrt(t/N);
+}
